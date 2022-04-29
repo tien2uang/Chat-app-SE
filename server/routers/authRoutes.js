35454a62d3bbf23bbbbd11f064b0ee3fa90b26fb
hfghfg123456 <81209064+hfghfg123456@users.noreply.github.com>
2022-04-29 12:31:41 +0700

    auth routes

---
+++ server/routers/authRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router();
+const authController = require('../controllers/authController')
+
+
+router.get('/sign-up', authController.signup_index);
+router.get('/log-in', authController.login_index);
+router.post('/sign-up', authController.signup_post);
+router.post('/log-in', authController.login_post);
+
+module.exports = router;
