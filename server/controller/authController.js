73e63c4e5af6cf346f1b21c5285173b817ff686d
hfghfg123456 <81209064+hfghfg123456@users.noreply.github.com>
2022-05-12 11:23:11 +0700

    Update authController.js

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,23 +1,16 @@
 const user = require('../model/User');
 
-const login_index = (req, res) => {
-    res.render('log-in', {anoucement: 'log-in'});
-}
-
-const signup_index = (req, res) => {
-    res.render('sign-up', {anoucement: 'sign-up'});
-}
 
 const signup_post = (req, res) => {
     const username = req.body.username;
     user.findOne({username: username})
         .then((oneuser) => {
             if (oneuser !== null) {
-                res.render('sign-up', {anoucement: 'this username has already taken'})
+                res.json({status: 'this username has already taken'})
             } else {
                 const newUser = new user(req.body);
                 newUser.save()
-                    .then(res.redirect('/'))
+                    .then(res.json({status: 'success sign up'})
                     .catch((err) => console.log(err));
             }
         })
@@ -30,20 +23,18 @@ const login_post = (req, res) => {
         .then((oneuser) => {
             if (oneuser !== null) {
                 if (oneuser.password !== req.body.password) {
-                    res.render('log-in', {anoucement: 'wrong password'})
+                    res.json({status: 'wrong password'})
                 } else {
-                    res.render('log-in', {anoucement: 'login success'})
+                    res.json({status: 'login success'})
                 }
             } else {
-                res.render('log-in', {anoucement: 'dont have this account'})
+                res.json({status: 'dont have this account'})
             }
         })
         .catch(err => console.log(err));
 }
 
 module.exports = {
-    login_index,
-    signup_index,
     signup_post,
     login_post
 }
