54cc5cfeb0d12dec3c58b6ec74078dd3aa9f60b1
tquang <duche345@gmail.com>
2022-05-14 15:26:37 +0700

    update authRouthes

--- server/routers/authRoutes.js
+++ server/routers/authRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const authController = require('../controller/authController')
 
 
-router.get('/sign-up', authController.signup_index);
-router.get('/log-in', authController.login_index);
+// router.get('/sign-up', authController.signup_index);
+// router.get('/log-in', authController.login_index);
 router.post('/sign-up', authController.signup_post);
 router.post('/log-in', authController.login_post);
 
