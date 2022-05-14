d8cf5cc3c2d5d622492f2f3e5bb4f0b97cb4dcc8
nguyenhoanUET <hoannguyendangmanh@gmail.com>
2022-05-14 18:18:54 +0700

    router

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,32 @@ import Friend from './pages/friendPage/friend';
 import {
   BrowserRouter as Router,
   Routes,
-  Route
+  Route, 
+  Navigate
 } from "react-router-dom";
+import { useContext } from 'react';
+import { AuthContext } from './context/AuthContext';
 
 
 function App() {
+  const { username } = useContext(AuthContext); 
   return (
-    <Router>
+     <Router>
        <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signIn" element={<SignIn />} />
-        <Route path="/signUp" element={<SignUp />} />
-        <Route path="/friend" element={<Friend />} />
-      </Routes>
-    </Router>
+         <Route path = "/" element = { username ? <Home /> : <SignIn />} />
+         <Route path = "/signIn" element = { username ? <Navigate  to = "/" /> : <SignIn />} />
+         <Route path = "/signUp" element = { username ? <Navigate  to = "/" /> : <SignUp />} />
+         <Route path = "/friend" element = { !username ? <Navigate  to = "/" /> : <Friend />} />
+       </Routes>
+     </Router>
+    // <Router>
+    //    <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/signIn" element={<SignIn />} />
+    //     <Route path="/signUp" element={<SignUp />} />
+    //     <Route path="/friend" element={<Friend />} />
+    //   </Routes>
+    // </Router>
   );
 }
 
