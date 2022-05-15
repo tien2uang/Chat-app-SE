04397de825b0c05d6fc22b866e90300d1b8f426a
tquang <duche345@gmail.com>
2022-05-15 16:00:45 +0700

    sua xiu syntax

--- client/src/components/leftbar/leftBar.jsx
+++ client/src/components/leftbar/leftBar.jsx
@@ -5,16 +5,16 @@ import Profile from "../../pages/profile/profile";
 
 export default function LeftBar() {
   return (
-    <div class="tab__1">
-      <div class="tab__1__list">
-        <button class="tab__1__list-items" title="Tin nhắn" onClick={() => {window.location.href = "http://localhost:3000"}}>
+    <div className="tab__1">
+      <div className="tab__1__list">
+        <button className="tab__1__list-items" title="Tin nhắn" onClick={() => {window.location.href = "http://localhost:3000"}}>
           <BsFillChatFill className="items" />
         </button>
-        <button class="tab__1__list-items" title="Bạn bè" onClick={() => {window.location.href = "http://localhost:3000/friend"}}>
+        <button className="tab__1__list-items" title="Bạn bè" onClick={() => {window.location.href = "http://localhost:3000/friend"}}>
           <FaUserFriends className="items" />
         </button>
         <Profile />
-        <button class="tab__1__list-items" title="Thoát" onClick={() => {window.location.href = "http://localhost:3000/signIn"}}>
+        <button className="tab__1__list-items" title="Thoát" onClick={() => {window.location.href = "http://localhost:3000/signIn"}}>
           <FaSignOutAlt className="items" />
         </button>
       </div>
