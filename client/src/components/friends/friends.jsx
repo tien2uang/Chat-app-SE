3d5b275394afd45b1a0ef6ec36e053ce3ad5992a
Quyet <20020467@vnu.edu.vn>
2022-05-17 01:39:22 +0700

    load conversation

--- client/src/components/friends/friends.jsx
+++ client/src/components/friends/friends.jsx
@@ -1,14 +1,22 @@
+import { useContext } from "react";
 import Conversation from "../Conversation/Conversation";
+import {AuthContext} from "../../context/AuthContext";
 import "./friends.css";
 
 export default function Friends() {
+
+  const {user} = useContext(AuthContext);
+  
   return (
     <div className="tab__2">
       <div className="tab__2__wrapper">
           <input placeholder="Search for your friends" className="friendChatMenu" />
           <div className="conversations">
-            <Conversation />
-            <Conversation />
+            {
+              user.friends.map(function(friendId) {
+                return <Conversation friendId={friendId} />
+              })
+            }
           </div>
       </div>
     </div>
