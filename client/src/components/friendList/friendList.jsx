5e65542fd1978b2d2f423e0df0d5aad592a22f2b
Quyet <20020467@vnu.edu.vn>
2022-05-20 00:32:24 +0700

    danh sach ban be

---
+++ client/src/components/friendList/friendList.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import "./friendList.css";
+import axios from "axios";
+import { FaRegTimesCircle} from "react-icons/fa";
+
+
+
+export default function FriendList({friendId}) {
+
+    const [user, setUser] = useState(null);
+    const [model, setModel] = useState(false);
+
+    useEffect(() => {
+        const getFriendInfo = async () => {
+        try {
+            const res = await axios.get("/users/" + friendId)
+            setUser(res.data);
+        } catch (error) {
+            console.log(error);
+        }
+        };
+        getFriendInfo();
+    })
+
+    const handletoggle = () => {
+        setModel(!model);
+    }
+
+    return (
+        <>
+
+        <div className="friend" onClick={handletoggle}>
+                <img
+                className="friendAvatar"
+                src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAA1BMVEUAAP+KeNJXAAAASElEQVR4nO3BgQAAAADDoPlTX+AIVQEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADwDcaiAAFXD1ujAAAAAElFTkSuQmCC"
+                alt=""
+                />
+                <h1 className="friendName">{user?.name} </h1>
+        </div>
+
+        {model && (
+            <div className="modal">
+            <div onClick={handletoggle} className="overlay"></div>
+            <div className="modal-content">
+              <h2>Profile</h2>
+              <h3>User name</h3>
+              <button className="close-modal" onClick={handletoggle}>
+                <FaRegTimesCircle className="exit" />
+              </button>
+            </div>
+          </div>
+        )}
+        
+        </>
+    );
+}
