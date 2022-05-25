9d332f28085bd8bd227d10cc5ce1b35dcaac026c
Quyet <20020467@vnu.edu.vn>
2022-05-25 10:32:18 +0700

    cap nhat

--- client/src/components/friends/friends.jsx
+++ client/src/components/friends/friends.jsx
@@ -1,24 +1,79 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import {AuthContext} from "../../context/AuthContext";
-import "./friends.css";
 import FriendList from "../friendList/friendList";
+import "./friends.css";
+import axios from "axios";
 
 export default function Friends() {
 
   const {user} = useContext(AuthContext);
+  const [status, setStatus] = useState(true);
+  const [searchResult, setSearchResult] = useState([]);
+  const [currentUser, setCurrentUser] = useState([]);
+
+  useEffect(() => {
+    const getFriendInfo = async () => {
+
+      try {
+        const res = await axios.get("/users/" + user._id + "/friends")
+        setCurrentUser(res.data)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getFriendInfo();
+  },[])
+
+  const search = async (e) => {
+    if(e.target.value.length !==0) {
+      
+      try {
+        const res = await axios.get("/users/" + user._id + "/friends")
+        const friends = res.data;
+        const filteredFriend = friends.filter(friend => 
+           friend.name.includes(e.target.value)
+        )
+        console.log(filteredFriend);
+        
+        setSearchResult(filteredFriend);
+  
+      } catch (error) {
+        console.log(error);
+      }
+
+      setStatus(false)
+    } else {
+      setStatus(true);
+    }
+  };
   
   return (
-    <div className="tab__2">
-      <div className="tab__2__wrapper">
-          <input placeholder="Search for your friends" className="friendChatMenu" />
-          <div className="conversations">
+    <div className="friendList">
+      <div className="friendListWrapper">
+        <input  
+              className="inputSearch" 
+              placeholder="Search for your friends"
+              onChange={search}
+        />
+        <button className="addFriend" title="thêm bạn bè">add</button>
+
+          {status?
+            <div className="friends">
             {
-              user.friends.map(function(friendId, key) {
-                return <FriendList friendId={friendId} key={key}/>
+              currentUser.map(function(friend, key) {
+                return <FriendList friendId={friend.id} key={key}/>
               })
             }
-      
-          </div>
+          </div> 
+          : <>{searchResult.length === 0? <div className="noResult">No result</div> 
+              : <div className="friends">
+                  {
+                    searchResult.map(function(friend, key) {
+                      return <FriendList friendId={friend.id} key={key}/>
+                    })
+                  }
+                </div>
+              }</>}
       </div>
     </div>
   );
